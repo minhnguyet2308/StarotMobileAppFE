4b4350d27c68c817d48d59a74700749be4e5a294
minhnguyet2308 <nguyetnmse172621@fpt.edu.vn>
2024-10-08 08:57:30 +0700

    fix proxy signin

--- app/(tabs)/(auth)/signin.tsx
+++ app/(tabs)/(auth)/signin.tsx
@@ -180,7 +180,7 @@ const SigninScreen: React.FC = () => {
         <TouchableOpacity
           className="bg-pink-100 mt-4 py-2 px-6 rounded-lg"
           disabled={!request}
-          onPress={() => promptAsync({ useProxy: true })} // Sử dụng proxy
+          onPress={() => promptAsync()} // Sử dụng proxy
         >
           <Text className="text-primary font-medium text-lg">
             Đăng nhập bằng Google

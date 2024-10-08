7d96d56814e4c17f6f933bd830a611d23bc04c85
minhnguyet2308 <nguyetnmse172621@fpt.edu.vn>
2024-10-08 10:32:01 +0700

    [update]:homeScreen

--- components/ViewContainer.tsx
+++ components/ViewContainer.tsx
@@ -1,5 +1,6 @@
 import { View, Image } from "react-native";
 import React, { ReactNode } from "react";
+import { ScrollView } from "react-native-gesture-handler";
 
 const cloundBt = require("@/assets/images/cloundbt.png");
 const cloundT = require("@/assets/images/cloundt.png");
@@ -8,30 +9,46 @@ const logo = require("@/assets/images/logo.png");
 interface ViewContainerProps {
   children: ReactNode;
   showLogo?: boolean;
+  showHeader?: boolean;
+  showFooter?: boolean;
 }
 
 const ViewContainer: React.FC<ViewContainerProps> = ({
   children,
   showLogo,
+  showFooter,
+  showHeader,
 }) => {
   return (
     <View className={`flex-1 justify-between bg-white`}>
-      <View className="relative  justify-center items-center">
+      {showHeader ? (
+        <View className="relative z-10 justify-center items-center">
+          <Image
+            source={cloundBt}
+            className={`w-screen h-44`}
+            resizeMode="cover"
+          />
+          {showLogo ? (
+            <View className="absolute">
+              <Image source={logo} resizeMode="contain" className="h-10" />
+            </View>
+          ) : null}
+        </View>
+      ) : null}
+      <ScrollView
+        className={`${showHeader ? "-mt-12" : null} ${
+          showFooter ? "-mb-40" : null
+        }`}
+      >
+        {children}
+      </ScrollView>
+      {showFooter ? (
         <Image
-          source={cloundBt}
-          className={`w-screen h-56`}
+          source={cloundT}
+          className={`w-full fixed h-52`}
           resizeMode="cover"
         />
-        {showLogo ? (
-          <View className="absolute">
-            <Image source={logo} resizeMode="contain" className="h-10"/>
-          </View>
-        ) : null}
-      </View>
-      <View className={`flex-1 justify-start items-center`}>
-        {children}
-      </View>
-      <Image source={cloundT} className={`w-full h-56`} resizeMode="cover" />
+      ) : null}
     </View>
   );
 };
